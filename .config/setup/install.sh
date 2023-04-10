7011b75932b71191e112fce743b467949716131f
Hassen Taidirt <hi@hassen.io>
2023-04-10 17:47:05 +0200

    Add install of Kubectl

--- .config/setup/install.sh
+++ .config/setup/install.sh
@@ -36,6 +36,12 @@ if ! command -v -- rvm > /dev/null 2>&1; then
 	sudo usermod -a -G rvm $(whoami)
 fi
 
+if ! command -v -- kubectl > /dev/null 2>&1; then
+	curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+	sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+	rm kubectl
+fi
+
 if ! command -v -- minikube > /dev/null 2>&1; then
 	curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 	sudo install minikube-linux-amd64 /usr/local/bin/minikube
